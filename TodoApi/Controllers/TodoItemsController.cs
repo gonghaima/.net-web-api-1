2b03af7b54734025fc66689959510e86b2766429
Steven Gong <gonghaima@hotmail.com>
2023-12-18 08:55:46 +1100

    get/post api

---
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TodoItemsController : ControllerBase
+{
+    private readonly ILogger<TodoItemsController> _logger;
+    private readonly TodoContext _context;
+
+    public TodoItemsController(TodoContext context, ILogger<TodoItemsController> logger)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+    {
+        var todoItem = await _context.TodoItems.FindAsync(id);
+
+        if (todoItem == null)
+        {
+            return NotFound();
+        }
+
+        return todoItem;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+    {
+        _context.TodoItems.Add(todoItem);
+        await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+        return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+    }
+}
+
